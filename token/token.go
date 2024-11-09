ea87a0eeabb87f48cc79cf5abf3597fa6dd4defe
gaurav-iitg <gauravkumar5089@gmail.com>
2024-11-09 15:37:43 +0530

    add: "new" function

--- token/token.go
+++ token/token.go
@@ -1,4 +1,56 @@
 package token
 
+type TokenType string
 
+type Token struct{
+	Type    TokenType
+	Literal string
+}
 
+type Lexer struct {
+	input string
+	position int // current position in input (points to current char)
+	readPosition int // current reading position in input (after current char)
+	ch byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar(){
+	if l.readPosition >= len(l.input){
+		l.ch = 0
+	}else{
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT = "INT" // 1343456
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET" 
+)
